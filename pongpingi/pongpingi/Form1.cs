61accb8dde889c0e0337dbf171ea2389110db076
yellowmonkey <roberto101125@gmail.com>
2017-04-20 11:19:02 +0200

    wersjapelna1

--- pongpingi/pongpingi/Form1.cs
+++ pongpingi/pongpingi/Form1.cs
@@ -21,7 +21,7 @@ namespace pongpingi
 
         public SerialPort port1, port2;
         public string a,b,d;
-        public int c;
+        public int c,f;
         
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -34,16 +34,11 @@ namespace pongpingi
             c = 1;
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void Form1_Activated(object sender, EventArgs e)
         {
-            c = 0;
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            if(c==1)
+            if (c == 1)
             {
-                if (b == "COM1")
+                if (a == "COM1")
                 {
                     d = "COM2";
                 }
@@ -51,17 +46,49 @@ namespace pongpingi
                 port2 = new SerialPort(d);
                 port2.Write(b);
                 textBox4.Text = b;
-                Thread.Sleep(100);
-                
-                
-
+                c = 0;
             }
+            if (f == 1)
+            {
+                
+                port1 = new SerialPort(a);
+                if (port1.ReadExisting() == "Ping" || port1.ReadExisting() == "Pong")
+                {
+                    if (port1.ReadExisting() == "Ping")
+                    {
+                        b = "Pong";
+                    }
+                    else b = "Ping";
+
+                    textBox2.Text = port1.ReadExisting();
+                    textBox4.Text = "";
+                }
+            }
+            Thread.Sleep(50);
+            if (textBox4.Text=="")
+            {
+                port2 = new SerialPort(d);
+                port2.Write(b);
+                textBox4.Text = b;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            c = 0;
+            f = 0;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+         
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             a = textBox3.Text;
             port1 = new SerialPort(a);
+            f = 1;
         }
     }
 }
