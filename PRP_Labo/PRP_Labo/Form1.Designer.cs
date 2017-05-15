79e291c12ff7d0ad1199313c4829286871b0ed8c
yellowmonkey <roberto101125@gmail.com>
2017-05-15 12:28:49 +0200

    zapis do pliku final

--- PRP_Labo/PRP_Labo/Form1.Designer.cs
+++ PRP_Labo/PRP_Labo/Form1.Designer.cs
@@ -30,19 +30,19 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.dane_port = new System.Windows.Forms.TextBox();
-            this.dane_szybkosc = new System.Windows.Forms.TextBox();
-            this.dane_dlugosc = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.connect = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.data_stopbits = new System.Windows.Forms.TextBox();
+            this.data_parity = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.dane_parzystosc = new System.Windows.Forms.TextBox();
-            this.dane_bitystopu = new System.Windows.Forms.TextBox();
-            this.zapisz = new System.Windows.Forms.Button();
-            this.polacz = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.data_bits = new System.Windows.Forms.TextBox();
+            this.data_baud = new System.Windows.Forms.TextBox();
+            this.data_port = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -59,18 +59,18 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.polacz);
-            this.tabPage1.Controls.Add(this.zapisz);
-            this.tabPage1.Controls.Add(this.dane_bitystopu);
-            this.tabPage1.Controls.Add(this.dane_parzystosc);
+            this.tabPage1.Controls.Add(this.connect);
+            this.tabPage1.Controls.Add(this.save);
+            this.tabPage1.Controls.Add(this.data_stopbits);
+            this.tabPage1.Controls.Add(this.data_parity);
             this.tabPage1.Controls.Add(this.label5);
             this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.dane_dlugosc);
-            this.tabPage1.Controls.Add(this.dane_szybkosc);
-            this.tabPage1.Controls.Add(this.dane_port);
+            this.tabPage1.Controls.Add(this.data_bits);
+            this.tabPage1.Controls.Add(this.data_baud);
+            this.tabPage1.Controls.Add(this.data_port);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -80,72 +80,39 @@
             this.tabPage1.UseVisualStyleBackColor = true;
             this.tabPage1.Enter += new System.EventHandler(this.tabPage1_Enter);
             // 
-            // tabPage2
+            // connect
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(549, 279);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.connect.Location = new System.Drawing.Point(91, 224);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(173, 24);
+            this.connect.TabIndex = 13;
+            this.connect.Text = "POŁĄCZ";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
             // 
-            // dane_port
+            // save
             // 
-            this.dane_port.Location = new System.Drawing.Point(232, 67);
-            this.dane_port.Name = "dane_port";
-            this.dane_port.Size = new System.Drawing.Size(171, 20);
-            this.dane_port.TabIndex = 0;
+            this.save.Location = new System.Drawing.Point(270, 224);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(173, 24);
+            this.save.TabIndex = 12;
+            this.save.Text = "ZAPISZ";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
             // 
-            // dane_szybkosc
+            // data_stopbits
             // 
-            this.dane_szybkosc.Location = new System.Drawing.Point(232, 93);
-            this.dane_szybkosc.Name = "dane_szybkosc";
-            this.dane_szybkosc.Size = new System.Drawing.Size(171, 20);
-            this.dane_szybkosc.TabIndex = 1;
+            this.data_stopbits.Location = new System.Drawing.Point(232, 172);
+            this.data_stopbits.Name = "data_stopbits";
+            this.data_stopbits.Size = new System.Drawing.Size(171, 20);
+            this.data_stopbits.TabIndex = 11;
             // 
-            // dane_dlugosc
+            // data_parity
             // 
-            this.dane_dlugosc.Location = new System.Drawing.Point(233, 146);
-            this.dane_dlugosc.Name = "dane_dlugosc";
-            this.dane_dlugosc.Size = new System.Drawing.Size(171, 20);
-            this.dane_dlugosc.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(134, 70);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(26, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Port";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(134, 96);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Szybkość transmisji";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(134, 122);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Parzystość";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(134, 149);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Długość ramki";
+            this.data_parity.Location = new System.Drawing.Point(232, 119);
+            this.data_parity.Name = "data_parity";
+            this.data_parity.Size = new System.Drawing.Size(171, 20);
+            this.data_parity.TabIndex = 10;
             // 
             // label5
             // 
@@ -156,39 +123,72 @@
             this.label5.TabIndex = 9;
             this.label5.Text = "Bity stopu";
             // 
-            // dane_parzystosc
+            // label4
             // 
-            this.dane_parzystosc.Location = new System.Drawing.Point(232, 119);
-            this.dane_parzystosc.Name = "dane_parzystosc";
-            this.dane_parzystosc.Size = new System.Drawing.Size(171, 20);
-            this.dane_parzystosc.TabIndex = 10;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(134, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Długość ramki";
             // 
-            // dane_bitystopu
+            // label3
             // 
-            this.dane_bitystopu.Location = new System.Drawing.Point(232, 172);
-            this.dane_bitystopu.Name = "dane_bitystopu";
-            this.dane_bitystopu.Size = new System.Drawing.Size(171, 20);
-            this.dane_bitystopu.TabIndex = 11;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Parzystość";
             // 
-            // zapisz
+            // label2
             // 
-            this.zapisz.Location = new System.Drawing.Point(270, 224);
-            this.zapisz.Name = "zapisz";
-            this.zapisz.Size = new System.Drawing.Size(173, 24);
-            this.zapisz.TabIndex = 12;
-            this.zapisz.Text = "ZAPISZ";
-            this.zapisz.UseVisualStyleBackColor = true;
-            this.zapisz.Click += new System.EventHandler(this.zapisz_Click);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Szybkość transmisji";
             // 
-            // polacz
+            // label1
             // 
-            this.polacz.Location = new System.Drawing.Point(91, 224);
-            this.polacz.Name = "polacz";
-            this.polacz.Size = new System.Drawing.Size(173, 24);
-            this.polacz.TabIndex = 13;
-            this.polacz.Text = "POŁĄCZ";
-            this.polacz.UseVisualStyleBackColor = true;
-            this.polacz.Click += new System.EventHandler(this.polacz_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port";
+            // 
+            // data_bits
+            // 
+            this.data_bits.Location = new System.Drawing.Point(233, 146);
+            this.data_bits.Name = "data_bits";
+            this.data_bits.Size = new System.Drawing.Size(171, 20);
+            this.data_bits.TabIndex = 2;
+            // 
+            // data_baud
+            // 
+            this.data_baud.Location = new System.Drawing.Point(232, 93);
+            this.data_baud.Name = "data_baud";
+            this.data_baud.Size = new System.Drawing.Size(171, 20);
+            this.data_baud.TabIndex = 1;
+            // 
+            // data_port
+            // 
+            this.data_port.Location = new System.Drawing.Point(232, 67);
+            this.data_port.Name = "data_port";
+            this.data_port.Size = new System.Drawing.Size(171, 20);
+            this.data_port.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(549, 279);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -210,18 +210,18 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TextBox dane_dlugosc;
-        private System.Windows.Forms.TextBox dane_szybkosc;
-        private System.Windows.Forms.TextBox dane_port;
+        private System.Windows.Forms.TextBox data_bits;
+        private System.Windows.Forms.TextBox data_baud;
+        private System.Windows.Forms.TextBox data_port;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox dane_parzystosc;
-        private System.Windows.Forms.TextBox dane_bitystopu;
-        private System.Windows.Forms.Button zapisz;
-        private System.Windows.Forms.Button polacz;
+        private System.Windows.Forms.TextBox data_parity;
+        private System.Windows.Forms.TextBox data_stopbits;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button connect;
     }
 }
 
--- PRP_Labo/PRP_Labo/Form1.cs
+++ PRP_Labo/PRP_Labo/Form1.cs
@@ -20,46 +20,48 @@ namespace PRP_Labo
 
         }
         
-        public void wczytaj_parametry()
+        public void load_parameters()
         {
 
             string filepath;
-            filepath = Path.GetDirectoryName(Application.ExecutablePath) + "\\plik.txt";
+            filepath = Path.GetDirectoryName(Application.ExecutablePath) + "\\" + "plik.txt"; // Universal pathname config
+            //MessageBox.Show(filepath); // Testing/debugging purposes
             
             if (File.Exists(@filepath))
             {
                 string[] parameters = File.ReadAllLines(@filepath);
-                
-                    dane_port.Invoke(new Action(delegate () {
-                        try  { dane_port.Text = parameters[0];  }
+                /* Reading data from file, try-catching the shit out of it */
+                    data_port.Invoke(new Action(delegate () {
+                        try  { data_port.Text = parameters[0];  }
                         catch { MessageBox.Show("Port name is either missing or corrupted!"); }  }));
-                    dane_szybkosc.Invoke(new Action(delegate () {
-                        try { dane_szybkosc.Text = parameters[1]; }
+                    data_baud.Invoke(new Action(delegate () {
+                        try { data_baud.Text = parameters[1]; }
                         catch { MessageBox.Show("Baudrate dara is either missing or corrupted!"); }  }));
-                    dane_parzystosc.Invoke(new Action(delegate () {
-                        try { dane_parzystosc.Text = parameters[2]; }
+                    data_parity.Invoke(new Action(delegate () {
+                        try { data_parity.Text = parameters[2]; }
                         catch { MessageBox.Show("Parity data is either missing or corrupted!"); }  }));
-                    dane_dlugosc.Invoke(new Action(delegate () {
-                        try { dane_dlugosc.Text = parameters[3]; }
+                    data_bits.Invoke(new Action(delegate () {
+                        try { data_bits.Text = parameters[3]; }
                         catch { MessageBox.Show("Amount of data bits is either missing or corrupted!"); }  }));
-                    dane_bitystopu.Invoke(new Action(delegate () {
-                        try { dane_bitystopu.Text = parameters[4]; }
+                    data_stopbits.Invoke(new Action(delegate () {
+                        try { data_stopbits.Text = parameters[4]; }
                         catch { MessageBox.Show("Stopbits data is either missing or corrupted!"); }  }));
             }
             else MessageBox.Show("File does not exist!");
         }
-        public void zapisz_parametry()
+        public void save_parameters()
         {
+            // Saving data
             string filepath;
             filepath = Path.GetDirectoryName(Application.ExecutablePath) + "\\plik.txt";
             if (File.Exists(@filepath))
             {
                 string[] t = new string[5];
-                t[0] = dane_port.Text;
-                t[1] = dane_szybkosc.Text;
-                t[2] = dane_parzystosc.Text;
-                t[3] = dane_dlugosc.Text;
-                t[4] = dane_bitystopu.Text;
+                t[0] = data_port.Text;
+                t[1] = data_baud.Text;
+                t[2] = data_parity.Text;
+                t[3] = data_bits.Text;
+                t[4] = data_stopbits.Text;
                 File.WriteAllLines(@filepath, t);
             }
             else MessageBox.Show("File does not exist!");
@@ -72,34 +74,37 @@ namespace PRP_Labo
 
         private void tabPage1_Enter(object sender, EventArgs e)
         {
-            wczytaj_parametry();
+            load_parameters();    // Upon entering tab with connection data load current setting from file
         }
 
-        private void zapisz_Click(object sender, EventArgs e)
+        private void save_Click(object sender, EventArgs e)
         {
-            zapisz_parametry();
+            save_parameters();     // Save to file
         }
 
-        private void polacz_Click(object sender, EventArgs e)
+        private void connect_Click(object sender, EventArgs e)
         {
-            SerialPort port_lab = new SerialPort(dane_port.Text);
-            port_lab.BaudRate = Convert.ToInt32(dane_szybkosc);
+            // Connection data conversion to appropriate values and types
+            SerialPort port_lab = new SerialPort(data_port.Text);
+            port_lab.BaudRate = Convert.ToInt32(data_baud.Text);
 
-            if (dane_parzystosc.Text == "none") port_lab.Parity = Parity.None;
-            else if (dane_parzystosc.Text == "odd") port_lab.Parity = Parity.Odd;
-            else if (dane_parzystosc.Text == "even") port_lab.Parity = Parity.Even;
+            if (data_parity.Text == "none") port_lab.Parity = Parity.None;
+            else if (data_parity.Text == "odd") port_lab.Parity = Parity.Odd;
+            else if (data_parity.Text == "even") port_lab.Parity = Parity.Even;
             else { MessageBox.Show("Wrong parity, setting parity to none!"); port_lab.Parity = Parity.None; }
+            /* Default parity set to avoid critical errors and their results */
 
-            port_lab.DataBits = Convert.ToInt32(dane_dlugosc.Text);
+            port_lab.DataBits = Convert.ToInt32(data_bits.Text);
 
-            if (dane_bitystopu.Text == "one") port_lab.StopBits = StopBits.One;
-            else if (dane_bitystopu.Text == "onepointfive") port_lab.StopBits = StopBits.OnePointFive;
-            else if (dane_bitystopu.Text == "two") port_lab.StopBits = StopBits.Two;
+            if (data_stopbits.Text == "one") port_lab.StopBits = StopBits.One;
+            else if (data_stopbits.Text == "onepointfive") port_lab.StopBits = StopBits.OnePointFive;
+            else if (data_stopbits.Text == "two") port_lab.StopBits = StopBits.Two;
             else
             {
                 MessageBox.Show("Wrong stopbits, setting stopbits to default!");
                 port_lab.StopBits = StopBits.One;
             }
+            /* Default stopbits set to avoid critical errors and their results */
         }
     }
 }
